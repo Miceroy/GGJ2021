8bdc804b1849cdcd13e4a44a4f033d78fa560537
Mikko Romppainen <mikko.romppainen@kamk.fi>
2021-01-30 09:29:47 +0200

    Koira + Pelaaja triggerit.

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -15,4 +15,23 @@ public class DogController : GameComponent
     {
         
     }
+
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.parent.tag == "Player")
+        {
+            Debug.Log("Dog near to player");
+        }
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit(Collider other)
+    {
+        if( other.gameObject.transform.parent.tag == "Player" )
+        {
+            Debug.Log("Dog far from player");
+
+        }
+    }
 }
